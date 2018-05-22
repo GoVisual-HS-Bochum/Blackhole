ec5aae9d1e5c18fc53d0713936bbcc4cacf8fb8f
KayLaesecke <kaylaesecke90@gmail.com>
2018-05-22 16:18:37 +0200

    Raum Übersicht erweitert
    Etagen und Raume werden geladen

--- src/main/webapp/app/room-overview/room-overview.component.ts
+++ src/main/webapp/app/room-overview/room-overview.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PositionRaumService } from '../entities/position-raum';
+import { Raum, RaumService } from '../entities/raum';
 
 @Component({
   selector: 'jhi-room-overview',
@@ -7,9 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomOverviewComponent implements OnInit {
 
-  constructor() { }
+  selectedLevel: number;
+  maxLevel = 0;
+  levels: number[] = new Array<number>();
+  raeume: Raum[];
+
+  constructor(private positionRaumService: PositionRaumService, private raumService: RaumService) { }
 
   ngOnInit() {
+    this.positionRaumService.query()
+      .map((positionRaum) => positionRaum.body)
+      .subscribe((positionRaum) => {
+        positionRaum.forEach((element) => {
+          if (element.etage > this.maxLevel) {
+            this.maxLevel = element.etage;
+          }
+        });
+
+        for (let i = 1; i <= this.maxLevel; i++) {
+          this.levels.push(i);
+        }
+
+        this.onLevelClick(0);
+      });
   }
 
+  onLevelClick(level: number) {
+    this.selectedLevel = this.levels[level];
+    this.getRäumeWithLevel();
+  }
+
+  getClass(level: number) {
+    if (this.levels[level] === this.selectedLevel) {
+      return 'btn btn-light active';
+    } else {
+      return 'btn btn-secondary';
+    }
+  }
+
+  private getRäumeWithLevel() {
+    this.raeume = new Array<Raum>();
+    this.positionRaumService.query()
+    .map((positionRaum) => positionRaum.body)
+    .subscribe((positionRaum) => {
+      positionRaum.forEach((element) => {
+        if (element.etage === this.selectedLevel) {
+          this.getRäumeAtLevel(element.id);
+        }
+      });
+    });
+  }
+
+  private getRäumeAtLevel(raumId: number) {
+    this.raumService.find(raumId).map( (r) => r.body).subscribe( (r) => this.raeume.push(r));
+  }
 }
